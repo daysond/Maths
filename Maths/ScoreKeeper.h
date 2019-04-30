5adbac3157a62ecc6f3a80132619dc86fafd1a10
Dayson Dong <Daysond@outlook.com>
2019-04-30 14:09:28 -0400

    added scorekeeper and done

---
+++ Maths/ScoreKeeper.h
@@ -0,0 +1,17 @@
+//
+//  ScoreKeeper.h
+//  Maths
+//
+//  Created by Dayson Dong on 2019-04-30.
+//  Copyright © 2019 Dayson Dong. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ScoreKeeper : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Maths/ScoreKeeper.m
@@ -0,0 +1,13 @@
+//
+//  ScoreKeeper.m
+//  Maths
+//
+//  Created by Dayson Dong on 2019-04-30.
+//  Copyright © 2019 Dayson Dong. All rights reserved.
+//
+
+#import "ScoreKeeper.h"
+
+@implementation ScoreKeeper
+
+@end
